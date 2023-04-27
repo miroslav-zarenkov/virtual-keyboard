b5d1dbb63c54ac6d862d07eb3c85164234547789
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-04-28 03:30:33 +0400

    refactor: add class names

--- src/js/ui.js
+++ src/js/ui.js
@@ -9,7 +9,9 @@ export default class PageStructure {
 
   createHeader() {
     this.header = document.createElement('header');
+    this.header.classList.add('header');
     const headerTitle = document.createElement('h1');
+    headerTitle.classList.add('header__title');
     headerTitle.textContent = 'Virtual Keyboard';
     this.header.appendChild(headerTitle);
     return this.header;
@@ -17,6 +19,7 @@ export default class PageStructure {
 
   createMain() {
     this.main = document.createElement('main');
+    this.main.classList.add('main');
     this.main.appendChild(this.createTextArea());
     this.main.appendChild(this.createKeyboard());
     return this.main;
@@ -24,18 +27,23 @@ export default class PageStructure {
 
   createTextArea() {
     this.textArea = document.createElement('textarea');
+    this.textArea.classList.add('main__textarea');
     return this.textArea;
   }
 
   createKeyboard() {
     this.keyboard = document.createElement('div');
+    this.keyboard.classList.add('main__keyboard');
+    this.keyboard.classList.add('keyboard');
     this.keyboard.textContent = 'qwertyuiop';
     return this.keyboard;
   }
 
   createFooter() {
     this.footer = document.createElement('footer');
+    this.footer.classList.add('footer');
     const footerInfo = document.createElement('p');
+    footerInfo.classList.add('footer__info');
     footerInfo.textContent = 'Created by miroslav-zarenkov';
     this.footer.appendChild(footerInfo);
     return this.footer;
