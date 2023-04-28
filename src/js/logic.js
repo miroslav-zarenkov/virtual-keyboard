b1db800d666f076e618c4abb6626fb18f6192614
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-04-29 01:20:43 +0400

    feat: add space, tab, return button functions

--- src/js/logic.js
+++ src/js/logic.js
@@ -3,8 +3,9 @@ import { newPage } from './ui';
 
 export default function addCharToTextArea() {
   const textArea = document.querySelector('textarea');
-  let char = '';
+  let char = "";
   const buttonCode = +this.value;
+  console.log(buttonCode)
   const language = newPage.getLanguage();
   for (let i = 0; i < jsonBtns.length; i++) {
     if (jsonBtns[i].role === 'character') {
@@ -18,7 +19,23 @@ export default function addCharToTextArea() {
         }
         break;
       }
+    }else if (jsonBtns[i].role === 'space') {
+      if (jsonBtns[i].keyCode === buttonCode) {
+        char = ' ';
+        break;
+      }
+    }else if (jsonBtns[i].role === 'enter') {
+      if (jsonBtns[i].keyCode === buttonCode) {
+        char = '\n';
+        break;
+      }
+    }else if (jsonBtns[i].role === 'tab'){
+      if (jsonBtns[i].keyCode === buttonCode) {
+        char = '\t';
+        break;
+      }
     }
   }
   textArea.textContent += char;
+  char = '';
 }
